95ff0c330084af38c6fc2645d5d8c5b11f9c44fd
David Boyer <mangr3n@gmail.com>
2021-03-15 07:10:16 -0400

    Initial files add

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,9 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+<style>
+    nav { display: flex; }
+    nav a { color: #ff3e00 }
+    nav a + a { margin-left: 1rem; }
+</style>
---
+++ src/routes/about.svelte
@@ -0,0 +1,3 @@
+<h1>About</h1>
+<p>SvelteKit is awesome!</p>
+<a href="/">Go back home</a>
